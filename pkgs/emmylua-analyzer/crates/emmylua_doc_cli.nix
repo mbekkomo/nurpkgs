1a6cdaa9955db0f93176a7e234ee1eeb3ece78b2
Komo <afiqquraisyzulkarnain@gmail.com>
2025-03-02 23:49:37 +0800

    rewrite

---
+++ pkgs/emmylua-analyzer/crates/emmylua_doc_cli.nix
@@ -0,0 +1,30 @@
+version:
+{
+  stdenv,
+  pkg-config,
+  rustPlatform,
+  fetchFromGitHub,
+  lib,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "emmylua_doc_cli";
+  inherit version;
+
+  src = fetchFromGitHub {
+    owner = "CppCXY";
+    repo = "emmylua-analyzer-rust";
+    rev = version;
+    hash = lib.fakeHash;
+  };
+
+  cargoHash = lib.fakeHash;
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ stdenv.cc.cc.lib ];
+
+  buildAndTestSubdir = "crates/${pname}";
+
+  postFixup = ''
+    patchelf --set-rpath "${stdenv.cc.cc.lib}/lib" $out/bin/${pname}
+  '';
+}
